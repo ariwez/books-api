c0eb1b9a07fba98bda71d8ff93526fd649e8d0ec
Ariel Wezik <wezik@red-sky.pl>
2021-07-03 14:28:19 +0200

    feat: add book model

--- books/api/models.py
+++ books/api/models.py
@@ -1,3 +1,16 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    book_id = models.CharField(max_length=32, unique=True)
+    title = models.CharField(max_length=256, blank=True)
+    authors = ArrayField(models.CharField(max_length=256), default=list, blank=True)
+    published_date = models.DateField(null=True)
+    categories = ArrayField(models.CharField(max_length=256), default=list, blank=True)
+    average_rating = models.FloatField(null=True)
+    ratings_count = models.PositiveIntegerField(null=True)
+    thumbnail = models.URLField(max_length=256, blank=True)
+
+    def __str__(self):
+        return f'{self.book_id} - {self.title}'
